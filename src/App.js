5ce3d8fc8af1f4dd2b32c63ea00fd167231aac3c
Jeon Seoungho <pcjoker10@gmail.com>
2024-06-22 14:46:55 +0900

    Create Customer.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import Customer from './componets/Customer';
+
+const customers = [
+{
+  'id': 1,
+  'image': 'https://picsum.photos/id/60/64/64',
+  'name': '홍길동',
+  'birthday': '961211',
+  'gender': '남자',
+  'job': '대학생'
+},
+{
+  'id': 2,
+  'image': 'https://picsum.photos/id/54/64/64  ',
+  'name': '전우치',
+  'birthday': '9960125',
+  'gender': '남자',
+  'job': '회사원'
+},
+{
+  'id': 1,
+  'image': 'https://picsum.photos/id/23/64/64',
+  'name': '성춘향',
+  'birthday': '960521',
+  'gender': '여자',
+  'job': '연예인'
+},
+]
 
 function App() {
   return (
-    <div className="gray-backgroud">
-      <img src={logo} lat="logo" />
-      <h2>Let's devolop management system!</h2>
+    <div>
+      {
+        customers.map(c => {
+          return (
+          <Customer
+            key={c.id}
+            id={c.id}
+            image={c.image}
+            name={c.name}
+            birthday={c.birthday}
+            gender={c.gender}
+            job={c.job}
+          />  
+          );
+        })
+      }
     </div>
   );
 }
